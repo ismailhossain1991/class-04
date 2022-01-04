02600125555b6a6a220011d4421c63f067feff1b
ismailhossain1991 <ismail017456@gmail.com>
2022-01-04 18:33:36 +0600

    file upload

---
+++ age calculator.js
@@ -0,0 +1,8 @@
+//Create an age calculator function
+
+function agecal(name, year){
+    return`My name is ${name} & I am ${2021 - year} years old`;
+}
+
+console.log(agecal('Ismail', 2000));
+console.log(agecal('Israk', 1991));
---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+// function dev(name = 'LFWF', age = 02, skill = 'js'){
+//     return(` my name is ${name} & i am ${age} years old & I love ${skill}`);
+// }
+
+// console.log(dev('Ahsan', 50, 'MongoDB'));
+
+
+
+
+
+
